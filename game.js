95a7323edeafc602efba4fb308b844afa6d8d618
GanemA <acmao@ucsc.edu>
2016-11-30 13:33:49 -0800

    Implemented a game loop. BUGGY will fix when I can

--- game.js
+++ game.js
@@ -7,9 +7,39 @@ var museum;
 var party;
 var currPlayer
 
+function player(myPlayerNum, myType){
+  this.playerNum = myPlayerNum;
+  this.type = myType;
+  this.skills = abilities[this.type];
+  this.inventory = [];
+  this.actionPoints = AP[this.type];
+}
+
+var abilities = {
+         "Thief": ["Pick Up","Drop","Unlock","lol whatever this one was"],
+    "Politician": [],
+       "Veteran": [],
+     "Professor": []
+};
+//Action points subject to change
+var AP = {
+  "Thief": 5,
+  "Politician": 4,
+  "Veteran": 5,
+  "Professor": 4
+}
+
+function fillParty(){
+  var roles = ["Thief", "Politician", "Veteran", "Professor"];
+  for(var i = 0; i < 4; i++){
+    party.push(new player(i+1, roles[i]))
+  }
+};
+
 //---------------------GAME STATES---------------------
 var states = {};
 var currState = "movementPhase";
+var currPlayerAP = [];
 
 states["movementPhase"] = new movementPhase();
 states["actionPhase"] = new actionPhase();
@@ -20,7 +50,20 @@ function stateManager(){
 
 function movementPhase(){
   this.begin = function(){
+    console.log("MovementPhase");
+    var input = new CanvasInput({
+    canvas: document.getElementById('The-Museum-Game')
+    });
 
+    $("#theButton").click(function(){
+        console.log(input.value());
+        currPlayerAP[currPlayer] -= 2;
+        currPlayer++;
+    })
+
+    for(var i = 0; i < party.length; i++){
+      currPlayerAP[i] = party[i].actionPoints;
+    }
   }
 
   this.draw = function(){
@@ -28,14 +71,35 @@ function movementPhase(){
   }
 
   this.update = function(){
+    console.log("current Player: " + currPlayer);
     this.draw();
+
+    if(currPlayer >= 4){
+      currPlayer = 0;
+      transitionState("actionPhase");
+    }else if(currPlayerAP[currPlayer] > 0){
+      console.log("current AP: " + currPlayerAP[currPlayer]);
+      //ask for input
+      //currPlayerAP[currPlayer]--;
+    }else{
+      console.log("No more points!");
+      currPlayer++;
+    }
   }
 };
 
 
 function actionPhase(){
   this.begin = function(){
+    console.log("ActionPhase");
+    var input = new CanvasInput({
+    canvas: document.getElementById('The-Museum-Game')
+    });
 
+    $("#theButton").click(function(){
+        console.log(input.value());
+        currPlayerAP[currPlayer]--;
+    })
   }
 
   this.draw = function(){
@@ -43,32 +107,35 @@ function actionPhase(){
   }
 
   this.update = function(){
+    console.log("current Player: " + currPlayer);
     this.draw();
+
+    if(currPlayer >= 4){
+      currPlayer = 0;
+      transitionState("movementPhase");
+    }else if(currPlayerAP[currPlayer] > 0){
+      //ask for input
+      //currPlayerAP[currPlayer]--;
+    }else{
+      Console.log("No more points!");
+      currPlayer++;
+    }
   }
 };
+
+function transitionState(newState){
+  currState = newState;
+  states[currState].begin();
+}
 //---------------------GAME FUNCTIONS------------------
 function init(){
   currStage = 1;
   museum = [];
   party = [];
   currPlayer = 0;
-
-};
-
-function fillParty(){
-  var roles = ["Thief", "Politician", "Veteran", "Professor"];
-  for(var i = 0; i < 4; i++){
-    party.push(new player(i+1, roles[i]))
-  }
+  fillParty();
+  transitionState("movementPhase");
 };
 
 init();
 setInterval(stateManager,FRAMES);
-
-var input = new CanvasInput({
-canvas: document.getElementById('The-Museum-Game')
-});
-
-$("#theButton").click(function(){
-    console.log(input.value())
-})
