ecd057bc271da049235f6613029fca1a5d8d90c5
Jacob <jacob.phong.le@gmail.com>
2016-12-04 18:54:02 -0800

    First

--- cardDatabase.js
+++ cardDatabase.js
@@ -90,10 +90,45 @@ function searchDatabase(index){
 //-----------------------CARD DATABASE STRUCTURE-------------------------
 var cardDatabase= {
          "example line": ["Room","stage 1 dialogue","stage 2 dialogue","stage 3 dialogue","stage 4 dialogue"],
-                   "L1": [new cardBase("Lobby","Welcome to the Museum pf History!","Experience our new Mysteries of the Inca exhibit! Available for a short time only!", "Room")],
-                   "C1": [new cardBase("Courtyard","Just a courtyard.","Why are you outside?", "Room")]
-                   "C2": [new cardBase("Courtyard","Just a courtyard.","Why are you outside?", "Room")]
-                   "C3": [new cardBase("Courtyard","Sigh","This was a mistake", "Room")]
+                   "L1": [new cardBase("The Lobby", ["Welcome to the Museum of History! Experience our new Mysteries of the Inca exhibit! Available for a short time only!", "Welcome to the Museum of History! Experience our new Mysteries of the Inca exhibit! Available for a short time only!", "There is no escape!", "There is no escape!"], [""], "Discovery Card")],
+                   "E1": [new cardBase("The Egyptian Exhibit",["The Egyptians were a once great civilization that had thrived through blistering sands and the burning Sun.", "The Egyptians were a once great civilization that had thrived through blistering sands and the burning Sun.","The Egyptians were a once great civilization that had thrived through blistering sands and the burning Sun.","The Egyptians were a once great civilization that had thrived through blistering sands and the burning Sun."],[""], "Discovery Card")],
+                   "E2": [new cardBase("The Egyptian Exhibit",["Cracked and worn with age, this burial urn has served its tenant well post mortem.", "Cracked and worn with age, this burial urn has served its tenant well post mortem.", "Cracked and worn with age, this burial urn has served its tenant well post mortem.", "Cracked and worn with age, this burial urn has served its tenant well post mortem."],["If picked up by a player, add the burial urn to their inventory.", "If picked up by a player, add the burial urn to their inventory.", "If picked up by a player, add the burial urn to their inventory.", "If picked up by a player, add the burial urn to their inventory.", "If picked up by a player, add the burial urn to their inventory."], "Item")],
+                   "E3": [new cardBase("The Egyptian Exhibit",["The Egyptians never did like having their tombs robbed.", "The Egyptians never did like having their tombs robbed.", "The Egyptians never did like having their tombs robbed.", "The Egyptians never did like having their tombs robbed."],["","Spawn a 2/2 Tomb Guardian in the current room.", "Spawn a 2/2 Tomb Guardian in the current room.","Spawn a 2/2 Tomb Guardian in the current room."], "Event")],
+                   "E4": [new cardBase("The Egyptian Exhibit",["The Egyptian Snake god represented chaos and opposed the gods of light and truth.", "The Egyptian Snake god represented chaos and opposed the gods of light and truth.", "The Egyptian Snake god represented chaos and opposed the gods of light and truth.", "The Egyptian Snake god represented chaos and opposed the gods of light and truth."],["Lose 2 AP for this turn only.", "Lose 2 AP for this turn only.", "Lose 2 AP for this turn only.", "Lose 2 AP for this turn only."], "Event")],
+                   "E5": [new cardBase("The Egyptian Exhibit",[""],[""], "")],
+                   "E6": [new cardBase("The Egyptian Exhibit",[""],[""], "")],
+                   "E7": [new cardBase("The Egyptian Exhibit",[""],[""], "")],
+                   "E8": [new cardBase("The Egyptian Exhibit",[""],[""], "")],
+                   "E9": [new cardBase("The Egyptian Exhibit",[""],[""], "")],
+                   "E10": [new cardBase("The Egyptian Exhibit",[""],[""], "")],
+                   "E11": [new cardBase("The Egyptian Exhibit",[""],[""], "")],
+                   "E12": [new cardBase("The Egyptian Exhibit",[""],[""], "")],
+                   "": [new cardBase("",[""],[""], "")],
+                   "": [new cardBase("",[""],[""], "")],
+                   "": [new cardBase("",[""],[""], "")],
+                   "": [new cardBase("",[""],[""], "")],
+                   "": [new cardBase("",[""],[""], "")],
+                   "": [new cardBase("",[""],[""], "")],
+                   "": [new cardBase("",[""],[""], "")],
+                   "": [new cardBase("",[""],[""], "")],
+                   "": [new cardBase("",[""],[""], "")],
+                   "": [new cardBase("",[""],[""], "")],
+                   "": [new cardBase("",[""],[""], "")],
+                   "": [new cardBase("",[""],[""], "")],
+                   "": [new cardBase("",[""],[""], "")],
+                   "": [new cardBase("",[""],[""], "")],
+                   "": [new cardBase("",[""],[""], "")],
+                   "": [new cardBase("",[""],[""], "")],
+                   "": [new cardBase("",[""],[""], "")],
+                   "": [new cardBase("",[""],[""], "")],
+                   "": [new cardBase("",[""],[""], "")],
+                   "": [new cardBase("",[""],[""], "")],
+                   "": [new cardBase("",[""],[""], "")],
+                   "": [new cardBase("",[""],[""], "")],
+
+                   "C1": [new cardBase("Courtyard","Just a courtyard.","Why are you outside?", "Room")],
+                   "C2": [new cardBase("Courtyard","Just a courtyard.","Why are you outside?", "Room")],
+                   "C3": [new cardBase("Courtyard","Sigh","This was a mistake", "Room")],
                    "C4": [new cardBase("Courtyard","Sigh","This was a mistake", "Room")]
 };
 //------------------------BOOKS-----------------------------------------
